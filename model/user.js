6f2c80b07d5488670c16afe5221cce6a0f52786b
Dulce María Silva Gutiérrez <sgmd682@gmail.com>
2020-10-31 11:34:17 -0600

    ultimocommit

--- model/user.js
+++ model/user.js
@@ -11,6 +11,16 @@ var userSchema = new Schema({
     type: String,
     required: true,
     minlength: 6
+  },nombre:{
+    type:String,
+    required:true
+  },apPat:{
+    type:String,
+    required:true
+  },apMat:{
+    type:String,
+    required:true
   }
+
 });
 module.exports = mongoose.model('User', userSchema);
--- routes/users.js
+++ routes/users.js
@@ -6,21 +6,36 @@ router.get('/',function (req, res, next) {
   res.send('respond with a resource');
 });
 /* LOGIN */
-router.post('/login',function (req, res, next) {
+router.post('/signup',function (req, res, next) {
   var user = new User({
     email:req.body.email,
-    password: req.body.password
+    password: req.body.password,
+    nombre:req.body.nombre,
+    apPat:req.body.apPat,
+    apMat:req.body.apMat
+
   });
 
   //Guarda un registro en Mongo
-  // user.save((err, response) => {
-  //   if (err)res.status(400).send(err);
-  //   res.status(200).send(response);
-  // });
+   user.save((err, response) => {
+     if (err)res.status(400).send(err);
+     res.status(200).redirect('/iniciarsesion');
+   });
   //Busca un registro mediante el email
-  User.findById(req.body.email, (err, user) => {
+ // User.findById(req.body.email, (err, user) => {
+ // if (err) res.status(400).send(err);
+ // res.status(200).send(user);
+  //});
+});
+
+router.post('/login',function (req, res, next) {
+  var email=req.body.email;
+  var password=req.body.password;
+  //Busca un registro mediante el email
+  User.findBy({email,password}, (err, user) => {
   if (err) res.status(400).send(err);
-  res.status(200).send(user);
+  res.status(200).redirect('/home');
+  
   });
 });
 
